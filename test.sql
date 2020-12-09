0d06618a62e55ffe6368bb18954b4e0aba75302f
anuragbisht1985 <49946265+anuragbisht1985@users.noreply.github.com>
2020-12-09 16:39:39 +0800

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,10 +1,4 @@
 create test(
 id tinyint unsigned default 0,
-name varchar(8),
-<<<<<<< HEAD
-address varchar(64) DEFAULT NULL
-=======
-address varchar(64) DEFAULT NULL,
-from_develop_branch varchar(4)
->>>>>>> develop
+name varchar(8)
 );
