f6bac9fd53d7c1436076d3b0a56ca1d399d25c87
AnuragB <anurag16.kec@gmail.com>
2021-05-24 15:06:24 +0800

    add new col

--- test.sql
+++ test.sql
@@ -3,6 +3,7 @@ id tinyint unsigned default 0,
 name varchar(8),
 address varchar(255),
 ip_count tinyint unsigned default 0,
-hash_count tinyint unsigned default 0);
+hash_count tinyint unsigned default 0),
+country_count tinyint unsigned default 0
 );
 
